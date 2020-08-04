b3801cccfb5123cfde8823307c22dfb63d6190a2
Christopher Weimer <chweimer@microsoft.com>
2020-08-04 21:13:18 +0000

    Fix error packet creation with dual command sets.

--- core/cmd_interface/cmd_interface_dual_cmd_set.c
+++ core/cmd_interface/cmd_interface_dual_cmd_set.c
@@ -23,8 +23,9 @@ static int cmd_interface_dual_cmd_set_process_request (struct cmd_interface *int
 	if (command_set == 0) {
 		return interface->intf_0->process_request (interface->intf_0, request);
 	}
-		
-	return interface->intf_1->process_request (interface->intf_1, request);
+	else {
+		return interface->intf_1->process_request (interface->intf_1, request);
+	}
 }
 
 static int cmd_interface_dual_cmd_set_issue_request (struct cmd_interface *intf, uint8_t command_id,
@@ -36,12 +37,33 @@ static int cmd_interface_dual_cmd_set_issue_request (struct cmd_interface *intf,
 		return CMD_HANDLER_INVALID_ARGUMENT;
 	}
 
-	return interface->intf_0->issue_request (interface->intf_0, command_id, request_params, buf, 
+	/* TODO: Handle multiple interfaces when creating requests.  This function should probably take
+	 * a cmd_set argument like the error packet handler. */
+	return interface->intf_0->issue_request (interface->intf_0, command_id, request_params, buf,
 		buf_len);
 }
 
+static int cmd_interface_dual_cmd_set_generate_error_packet (struct cmd_interface *intf,
+	struct cmd_interface_request *request, uint8_t error_code, uint32_t error_data, uint8_t cmd_set)
+{
+	struct cmd_interface_dual_cmd_set *interface = (struct cmd_interface_dual_cmd_set*) intf;
+
+	if (interface == NULL) {
+		return CMD_HANDLER_INVALID_ARGUMENT;
+	}
+
+	if (cmd_set == 0) {
+		return interface->intf_0->generate_error_packet (interface->intf_0, request, error_code,
+			error_data, cmd_set);
+	}
+	else {
+		return interface->intf_1->generate_error_packet (interface->intf_1, request, error_code,
+			error_data, cmd_set);
+	}
+}
+
 /**
- * Initialize a command interface instance with two command sets supported. Requests from each 
+ * Initialize a command interface instance with two command sets supported. Requests from each
  * command set get routed to the appropiate command interface. Issuing requests from this interface
  * defaults to the first command set.
  *
@@ -58,11 +80,14 @@ int cmd_interface_dual_cmd_set_init (struct cmd_interface_dual_cmd_set *intf,
 		return CMD_HANDLER_INVALID_ARGUMENT;
 	}
 
+	memset (intf, 0, sizeof (struct cmd_interface_dual_cmd_set));
+
 	intf->intf_0 = intf_0;
 	intf->intf_1 = intf_1;
 
 	intf->base.process_request = cmd_interface_dual_cmd_set_process_request;
 	intf->base.issue_request = cmd_interface_dual_cmd_set_issue_request;
+	intf->base.generate_error_packet = cmd_interface_dual_cmd_set_generate_error_packet;
 
 	return 0;
 }
