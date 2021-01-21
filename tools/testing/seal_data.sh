39562ddef18b460fe44253790535eb8e4c97a31a
Christopher Weimer <chweimer@microsoft.com>
2021-01-21 17:01:51 +0000

    Update to sealing test script.
    Allow the ciphertext length and RSA seed length to be specified.  Provide easy mechanisms to generate maximum length payloads.

--- tools/testing/seal_data.sh
+++ tools/testing/seal_data.sh
@@ -15,6 +15,13 @@ else
 	privkey=$4
 fi
 
+if [ -z "$CIPHER_LEN" ]; then
+	CIPHER_LEN=32
+fi
+if [ -z "$SEED_LEN" ]; then
+	SEED_LEN=318		# Maximum length for RSA3k key
+fi
+
 cert=$3
 key=`mktemp -p .`
 openssl x509 -inform DER -outform PEM -noout -pubkey -in $cert > $key
@@ -65,7 +72,7 @@ if [ "$1" = "1" ]; then
 else
 	ecc=''
 	seed=`mktemp -p .`
-	head -c 32 /dev/random > $seed
+	head -c $SEED_LEN /dev/random > $seed
 
 	if [ "$2" = "2" ]; then
 		openssl pkeyutl -in $seed -encrypt -inkey $key -pubin -pkeyopt rsa_padding_mode:oaep -pkeyopt rsa_oaep_md:sha256 -pkeyopt rsa_mgf1_md:sha256 -out seed.bin
@@ -87,6 +94,11 @@ else
 	echo "Private Key: $ecc"
 fi
 
+seed_len=`stat -c %s seed.bin`
+if [ $CIPHER_LEN -lt 0 ]; then
+	let 'CIPHER_LEN = -(CIPHER_LEN + seed_len)'
+fi
+
 seed_hex=`cat $seed | xxd -p | tr -d '\n'`
 echo "Seed: $seed_hex"
 
@@ -101,7 +113,7 @@ if [ $? -ne 0 ]; then
 fi
 
 sealing="sealing.bin"
-head -c 32 /dev/random > cipher.bin
+head -c $CIPHER_LEN /dev/random > cipher.bin
 
 head -c 32 /dev/zero > $sealing
 if [ -n "$PMR0" ]; then
@@ -124,8 +136,19 @@ else
 	head -c 32 /dev/zero >> $sealing
 fi
 
-head -c 64 /dev/zero >> $sealing
-head -c 64 /dev/zero >> $sealing
+head -c 32 /dev/zero >> $sealing
+if [ -n "$PMR3" ]; then
+	echo $PMR3 | xxd -r -p >> $sealing
+else
+	head -c 32 /dev/zero >> $sealing
+fi
+
+head -c 32 /dev/zero >> $sealing
+if [ -n "$PMR4" ]; then
+	echo $PMR4 | xxd -r -p >> $sealing
+else
+	head -c 32 /dev/zero >> $sealing
+fi
 
 sign=`cat $sign_key | xxd -p | tr -d '\n'`
 
