c890c7800c7186a91847ad98a9900db15db690d0
Christopher Weimer <chweimer@microsoft.com>
2022-01-31 04:07:04 +0000

    Add AES test vectors for CBC encryption.

--- core/testing/crypto/aes_testing.h
+++ core/testing/crypto/aes_testing.h
@@ -8,6 +8,7 @@
 #include <stddef.h>
 
 
+/* Test data for AES-GCM operations. */
 extern const uint8_t AES_KEY[];
 #define AES_KEY_LEN (256 / 8)
 
@@ -27,6 +28,31 @@ extern const uint8_t AES_RSA_PRIVKEY_DER[];
 extern const size_t AES_RSA_PRIVKEY_DER_LEN;
 
 extern const uint8_t AES_RSA_PRIVKEY_GCM_TAG[];
+
+
+/* Test data for AES-CBC operations.  Values are taken from NIST test vectors. */
+#define	AES_TESTING_CBC_IV_LEN	16
+
+extern const uint8_t AES_TESTING_CBC_SINGLE_BLOCK_KEY[];
+extern const uint8_t AES_TESTING_CBC_SINGLE_BLOCK_IV[];
+
+extern const uint8_t AES_TESTING_CBC_SINGLE_BLOCK_PLAINTEXT[];
+extern const uint8_t AES_TESTING_CBC_SINGLE_BLOCK_CIPHERTEXT[];
+extern const size_t AES_TESTING_CBC_SINGLE_BLOCK_LEN;
+
+extern const uint8_t AES_TESTING_CBC_MULTI_BLOCK_KEY[];
+extern const uint8_t AES_TESTING_CBC_MULTI_BLOCK_IV[];
+
+extern const uint8_t AES_TESTING_CBC_MULTI_BLOCK_PLAINTEXT[];
+extern const uint8_t AES_TESTING_CBC_MULTI_BLOCK_CIPHERTEXT[];
+extern const size_t AES_TESTING_CBC_MULTI_BLOCK_LEN;
+
+extern const uint8_t AES_TESTING_CBC_LONG_DATA_KEY[];
+extern const uint8_t AES_TESTING_CBC_LONG_DATA_IV[];
+
+extern const uint8_t AES_TESTING_CBC_LONG_DATA_PLAINTEXT[];
+extern const uint8_t AES_TESTING_CBC_LONG_DATA_CIPHERTEXT[];
+extern const size_t AES_TESTING_CBC_LONG_DATA_LEN;
 
 
 #endif /* AES_TESTING_H_ */
