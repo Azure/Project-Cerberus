0e8914c6440618b3074d4ecb0664e2dedb9aff9e
Dan Pulsipher <Daniel.Pulsipher@microsoft.com>
2022-06-29 21:59:38 +0000

    Merged PR 1967: Move the command for removing profile files to run every build
    This change will remove the files generated by running the coverage target. This prevents the warnings generated indicating the .gcda files are being overwritten.
    
    Related work items: #1235

--- cmake/Coverage.cmake
+++ cmake/Coverage.cmake
@@ -23,6 +23,11 @@ find_program( GENHTML_PATH genhtml )
 set(COVERAGE_COMPILER_FLAGS "-g -O0 --coverage -fprofile-arcs -ftest-coverage"
 	CACHE INTERNAL "")
 
+add_custom_command(
+ 	TARGET ${TARGET_NAME}
+ 	COMMAND ${LCOV_PATH} --directory . --zerocounters -q
+ 	)
+
 set(CMAKE_CXX_FLAGS_COVERAGE
 	${COVERAGE_COMPILER_FLAGS}
 	CACHE STRING "Flags used by the C++ compiler during coverage builds."
@@ -41,30 +46,23 @@ endif()
 
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${COVERAGE_COMPILER_FLAGS}")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${COVERAGE_COMPILER_FLAGS}")
-
+	
 function(SETUP_TARGET_FOR_COVERAGE)
 
-    set(options NONE)
-    set(oneValueArgs NAME)
-    set(multiValueArgs EXECUTABLE EXECUTABLE_ARGS DEPENDENCIES)
-    cmake_parse_arguments(Coverage "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+	set(options NONE)
+	set(oneValueArgs NAME)
+	set(multiValueArgs EXECUTABLE EXECUTABLE_ARGS DEPENDENCIES)
+	cmake_parse_arguments(Coverage "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
 
-    add_custom_target(${Coverage_NAME}
-        # Cleanup lcov
-        COMMAND ${LCOV_PATH} --directory . --zerocounters
+	add_custom_target(${Coverage_NAME}
 
-        # Run tests
-        COMMENT "Running unit tests and generating coverage report"
-        COMMAND ${Coverage_EXECUTABLE}
+		# Run tests
+		COMMENT "Running unit tests and generating coverage report"
+		COMMAND ${Coverage_EXECUTABLE}
 
-        COMMAND ${LCOV_PATH} -c -d ${PROJECT_BINARY_DIR}/CMakeFiles -o coverage.info -q
-        COMMAND ${LCOV_PATH} --remove coverage.info "*/testing/*" "*/mbedtls*" -o coverage.info -q
-        COMMAND ${GENHTML_PATH} coverage.info --output-directory ${PROJECT_BINARY_DIR}/coverage_report -q
-    )
-
-    add_custom_command(TARGET ${Coverage_NAME} POST_BUILD
-        COMMAND ;
-        COMMENT "Coverage report can be found in coverage_report"
-    )
+		COMMAND ${LCOV_PATH} -c -d ${PROJECT_BINARY_DIR}/CMakeFiles -o coverage.info -q
+		COMMAND ${LCOV_PATH} --remove coverage.info "*/testing/*" "*/mbedtls*" -o coverage.info -q
+		COMMAND ${GENHTML_PATH} coverage.info --output-directory ${PROJECT_BINARY_DIR}/coverage_report -q
+		)
 
 endfunction() # SETUP_TARGET_FOR_COVERAGE
