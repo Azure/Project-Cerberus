7e5b3cd1241c30ba602960d70336be67116082d4
Christopher Weimer <chweimer@microsoft.com>
2023-02-28 00:10:02 +0000

    Increase the maximum number of CuTest tests supported.

--- projects/linux/testing/platform_CuTest.h
+++ projects/linux/testing/platform_CuTest.h
@@ -10,7 +10,7 @@
 #define	platform_CuStringAppendFormat		CuStringAppendFormat
 
 /* The maximum number of tests supported on the platform. */
-#define	PLATFORM_MAX_TEST_CASES				16384
+#define	PLATFORM_MAX_TEST_CASES				(32 * 1024)
 
 
 #endif /* PLATFORM_CUTEST_H_ */
