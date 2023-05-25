952345f4486a03137fbeb53d74787d3820281e67
Tanner Stevens <tstevens@microsoft.com>
2023-05-25 23:12:58 +0000

    Merged PR 2558: Updated Default FreeRTOS ISR Yield Handler
    The FreeRTOS yield flag should not go unhandled.  Default, unoptimized abstracted API should handle the context switch inlined.

---
+++ projects/freertos/update_yield_default.c
@@ -0,0 +1,15 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+#include "FreeRTOS.h"
+
+
+/* Unoptimized implementation of an inlined context switch as indicated by the ISR API.  This file
+ * can be used when no special handling of the flag has been implemented. */
+void freertos_isr_update_yield (BaseType_t yield)
+{
+	if (yield == pdTRUE) {
+		vTaskSwitchContext ();
+	}
+}
+
