17b0c592af56c24f6abac51ba550871bfe14e9f2
Lakshmi Praveen Kopparthi <lkopparthi@microsoft.com>
2023-08-05 09:15:56 +0000

    Merged PR 2723: Add module ID for host firmware interface
    Add module ID for host firmware interface
    
    Related work items: #2966

--- core/status/module_id.h
+++ core/status/module_id.h
@@ -124,6 +124,7 @@ enum {
 	ROT_MODULE_DICE_UEID_EXTENSION = 0x0070,			/**< Extension handler for TCG DICE Ueid extensions. */
 	ROT_MODULE_DME_EXTENSION = 0x0071,					/**< Extension handler for DME extensions. */
 	ROT_MODULE_DME_STRUCTURE = 0x0072,					/**< Parsing and management of the DME structure. */
+	ROT_MODULE_HOST_FIRMWARE_IMAGE = 0x0073,			/**< Interface to Host firmware image operations. */
 };
 
 
