309a1d66e337ebefca9ccc8704762cdf359aaf0c
Christopher Weimer <chweimer@microsoft.com>
2024-10-21 18:35:13 +0000

    Add new log message for watchdog start failures.

--- core/system/system_logging.h
+++ core/system/system_logging.h
@@ -20,7 +20,8 @@ enum {
 	SYSTEM_LOGGING_DEVICE_UNLOCKED,			/**< An unlock policy has been applied to the device. */
 	SYSTEM_LOGGING_LOCK_STATE_FAIL,			/**< An error occurred attempting to make the lock state consistent. */
 	SYSTEM_LOGGING_TOKEN_INVALIDATE_FAIL,	/**< Failed to invalidate a consumed unlock token. */
-	SYSTEM_LOGGING_REFRESH_WATCHDOG_FAIL,	/**< Failed to refresh the hardware watchdog counter. */
+	SYSTEM_LOGGING_REFRESH_WATCHDOG_FAIL,	/**< Failed to refresh the hardware watchdog timer. */
+	SYSTEM_LOGGING_START_WATCHDOG_FAIL,		/**< Failed to start the hardware watchdog timer. */
 };
 
 /**
